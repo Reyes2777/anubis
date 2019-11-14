8ac0efe67912a6796fc8ed5d4d1f5c66a247fe1b
Jonathan Reyes <jonathan.reyes2777@gmail.com>
2019-11-13 22:50:55 -0500

    create project auth anubis

---
+++ authentication/auth.py
@@ -0,0 +1,50 @@
+import typing
+
+from starlette.applications import Starlette
+from starlette.authentication import requires
+from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.responses import JSONResponse, PlainTextResponse
+
+from starlette_auth_toolkit.base.backends import BaseBasicAuth
+from starlette_auth_toolkit.cryptography import PBKDF2Hasher
+
+# Password hasher
+hasher = PBKDF2Hasher()
+
+
+# Example user Model
+class User(typing.NamedTuple):
+    username: str
+    password: str
+
+
+# Faker user Storage
+USERS = {
+    "alice": User(username="alice", password=hasher.make_sync("alicepwd")),
+    "bob": User(username="bob", password=hasher.make_sync("bobpwd"))
+}
+
+
+# Authentication Backend
+class BasicAuth(BaseBasicAuth):
+    async def find_user(self, username:str):
+        return USERS.get(username)
+
+    async def verify_password(self, user: User, password:str):
+        return hasher.verify(password, user.password)
+
+
+# Application
+app = Starlette()
+
+app.add_middleware(
+    AuthenticationMiddleware,
+    backend=BasicAuth(),
+    on_error=lambda _, exc: PlainTextResponse(str(exc), status_code=401),
+)
+
+
+@app.route("/protected")
+@requires("authenticated")
+async def protected(request):
+    return JSONResponse({"message": f"Hello, {request.user.username} !!!!"})
