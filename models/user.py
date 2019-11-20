1bd2c1ac2fdd2c4dd1a946e568bbf85435aa3a10
Jonathan Reyes <jonathan.reyes2777@gmail.com>
2019-11-19 22:42:24 -0500

    refactor model user for use mongoengine

--- models/user.py
+++ models/user.py
@@ -1,30 +1,9 @@
 from typing import Dict
 
-from db import DB
+from mongoengine import Document, StringField, EmailField
 
 
-class User:
-    _db = DB()
-
-    def __init__(self, data: Dict):
-        self._id = data.get('_id')
-        self._username = data.get('username')
-        self._email = data.get('email')
-        self._password = data.get('password')
-
-    @property
-    def id(self):
-        return self._id
-
-    @property
-    def username(self):
-        return self._username
-
-    @property
-    def email(self):
-        return self._email
-
-    @property
-    def password(self):
-        return self._password
-
+class User(Document):
+        email = EmailField(required=True)
+        password = StringField(required=True)
+        username = StringField()
